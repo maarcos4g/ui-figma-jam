d82dbd7431e1049c75f71ce4fe1c9e5ff96465df
maarcos4g <marcospaullo552@gmail.com>
2023-01-27 10:35:59 -0300

    exibindo quadrados e fazendo conexão entre eles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
-import ReactFlow, { Background, Controls } from 'reactflow'
+import ReactFlow, { addEdge, Background, Connection, ConnectionMode, Controls, Node, useEdgesState, useNodesState } from 'reactflow'
+import { zinc } from 'tailwindcss/colors'
 import 'reactflow/dist/style.css'
 
-import { zinc } from 'tailwindcss/colors'
+import { Square } from './components/nodes/Square'
+import { useCallback } from 'react'
+
+const NODE_TYPES = {
+  square: Square,
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {},
+  },
+] satisfies Node[]
 
 function App() {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES)
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges))
+  }, [])
 
   return (
     <div className='w-screen h-screen'>
-      <ReactFlow>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        connectionMode={ConnectionMode.Loose}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
         <Background
           gap={12}
           size={2}
